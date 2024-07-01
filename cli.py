199564bf9a9b85b2f5707d0ded7653f7890d14ec
Your Name~ <you@example.com>
2024-07-01 23:21:25 +0300

    add data axtracter

--- cli.py
+++ cli.py
@@ -1,6 +1,10 @@
 import argparse
-
+from src.data_extracter import DataExtractor
 parser = argparse.ArgumentParser(description="Домен")
 parser.add_argument("-n", help="Введите домен компании")
 args = parser.parse_args()
-print(args.n)
+# print(args.n)
+
+dt = DataExtractor("gazprom")
+dt.save_docs()
+
